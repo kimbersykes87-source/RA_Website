79f7d50f9c6046a1a838118cd857b2c9a86115ff
kimbersykes87 <kimbersykes87@gmail.com>
2026-01-04 12:56:31 -0700

    Update to v5 Apps Script - doGet function enables CORS

--- soi-site/js/config.js
+++ soi-site/js/config.js
@@ -13,7 +13,7 @@
 export const CONFIG = {
   // Google Apps Script web app endpoint URL
   // Format: https://script.google.com/macros/s/YOUR_SCRIPT_ID/exec
-  APPS_SCRIPT_ENDPOINT: 'https://script.google.com/macros/s/AKfycbyZCnBFJZDsPwpE0NnhMRSbj0NDQGrPQ3CvW4IFLFRqVGhVN1sYPmYnI8m2US84gS_2Jw/exec',
+  APPS_SCRIPT_ENDPOINT: 'https://script.google.com/macros/s/AKfycbwuiWeVlLTun8RpKx2NvhwnMKFBLoU6e5WbywFqsbm9HMb21JhofPVFj-_uI58DGN1G/exec',
   
   // Form metadata
   FORM_NAME: 'Statement of Intent 2026'
